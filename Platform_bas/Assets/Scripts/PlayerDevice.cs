9175038793f47caf4b6adfd2974d0e73585b0c63
GitMats85 <mats.tauholm@lbs.se>
2024-03-13 16:34:55 +0100

    lite nytt

--- Platform_bas/Assets/Scripts/PlayerDevice.cs
+++ Platform_bas/Assets/Scripts/PlayerDevice.cs
@@ -6,14 +6,13 @@ public class PlayerDevice : MonoBehaviour
 {
     public GameObject player1Prefab;
     public GameObject player2Prefab;
-    // Start is called before the first frame update
+
     void Start()
     {
         var p1 = PlayerInput.Instantiate(player1Prefab, controlScheme: "KeyboardWASD", pairWithDevice: Keyboard.current);
         var p2 = PlayerInput.Instantiate(player2Prefab, controlScheme: "KeyboardArrows", pairWithDevice: Keyboard.current);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
